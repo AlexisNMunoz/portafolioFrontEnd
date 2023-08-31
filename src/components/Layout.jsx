dddc4c049605c93bad0333f62cc3d442e42a790d
AlexisNMunoz <alexisnahuelmunioz@gmail.com>
2023-08-31 17:08:25 -0300

    fex cv

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,7 +10,7 @@ function Layout() {
 
                     <div className=" flex gap-5 items-center">
                         <button className=" border border-black/80 p-2 rounded-md font-bold text-sm text-black/80 hover:bg-gray-100 transition-colors">
-                            <a href="/CV2023.pdf" target="_blank" rel="noopener noreferrer" download="CV2023.pdf"
+                            <a href="/CV2023.pdf" target="_blank" rel="noopener noreferrer" download="CV-Alexis-Muñoz-FrontEnd-Developer.pdf"
                             >Descargar CV</a>
                         </button>
 
